fa66d549615c0501d6a6c1ceaeefa5f52f4f1c26
VansiluKodikara <vansilu08@gmail.com>
2026-02-10 19:46:37 +0530

    Initial commit

--- src/main/java/service/custom/impl/ItemService.java
+++ src/main/java/service/custom/impl/ItemService.java
@@ -1,4 +1,14 @@
 package service.custom.impl;
 
+import model.Item;
+
+import java.util.List;
+
 public interface ItemService {
+    boolean addItem(Item item);
+    boolean updateItem(Item item);
+    boolean deleteItem(String id);
+    Item searchItemById(String id);
+
+    List<Item> getAll();
 }
--- src/main/java/service/custom/impl/OrderService.java
+++ src/main/java/service/custom/impl/OrderService.java
@@ -1,4 +1,14 @@
 package service.custom.impl;
 
+import model.Order;
+
+import java.util.List;
+
 public interface OrderService {
+    boolean addOrder(Order order);
+    boolean updateOrder(Order order);
+    boolean deleteOrder(String id);
+    Order searchOrderById(String id);
+
+    List<Order> getAll();
 }
