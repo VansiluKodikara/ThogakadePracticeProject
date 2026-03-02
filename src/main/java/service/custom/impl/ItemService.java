b4f56936c30183dfe5998ceec8e86d7434b119fd
VansiluKodikara <vansilu08@gmail.com>
2026-03-02 10:36:04 +0530

    Initial commit

--- src/main/java/service/custom/impl/ItemService.java
+++ src/main/java/service/custom/impl/ItemService.java
@@ -2,10 +2,11 @@ package service.custom.impl;
 
 import model.Item;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ItemService {
-    boolean addItem(Item item);
+    boolean addItem(Item item) throws SQLException;
     boolean updateItem(Item item);
     boolean deleteItem(String id);
     Item searchItemById(String id);
